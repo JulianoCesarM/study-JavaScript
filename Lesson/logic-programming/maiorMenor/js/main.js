4e4757de6dd699081bab19ed51b0f80fc4298463
Juliano Cesar Magoga <julianoc.m@hotmail.com>
2023-10-24 15:40:12 -0300

    feat: função maior menor

---
+++ Lesson/logic-programming/maiorMenor/js/main.js
@@ -0,0 +1,16 @@
+// lógica de programação básica
+// function max(num1, num2) {
+//   if (num1 > num2) {
+//     console.log("O primeiro número é maior")
+//   } else if (num2 > num1) {
+//     console.log("O segundo número é maior")
+//   } else {
+//     console.log("Números com valores iguais")
+//   }
+// }
+// max(2, 5)
+
+// lógica de programação avançado
+// mesmo código porem bem mais simples
+const max = (num1, num2) => (num1 > num2 ? num1 : num2)
+console.log(max(12, 15))
